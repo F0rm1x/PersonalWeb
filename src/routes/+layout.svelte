8730afeb5d0c16092e3925e5581fa718293aefd5
F0rm1x <dformanekperson@proton.me>
2025-01-08 23:03:01 +0100

    Added menu to layout, added new test route - will be used in a new branch, added container for future changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <script>
   import "../app.css";
 </script>
-
+<nav class="bg-black pl-9 pr-9">
+  <div class="space-x-4 md:flex">
+    <a href="/" class="hover:bg-gray-500 space-x-4 p-4 text-xl text-white hover:text-gray-900">Home</a>
+    <a href="/test" class="hover:bg-gray-500 space-x-4 p-4 text-xl text-white hover:text-gray-900">Test</a>
+  </div>
+</nav>
+<main>
 <slot />
+</main>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.black);
+    color: theme('colors.gray.800')
+  }
+</style>
