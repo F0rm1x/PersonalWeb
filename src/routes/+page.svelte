d11a50bcfbdc3854488721257e0d2870f2c4e030
F0rm1x <dformanekperson@proton.me>
2025-07-10 10:47:21 +0200

    Merged commits from main, added more of a text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,28 @@
-<!--TODO: Add responsivity-->
+  <!--TODO: Add responsivity-->
 <div class="grid place-items-center h-screen">
-  <div class="pl-10 bg-gray-100 bg-opacity-70 rounded-md w-4/5 md:w-1/2 lg:w-1/2">
-    <h3 class="text-center text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold">F0rm1x</h3>
-    <div class="flex flex-wrap justify-center space-x-4 md:space-x-6 text-xl sm:text-lg md:text-xl lg:text-2xl font-bold">
-      <a href="#game-developer" class="flex items-center text-gray-700 hover:text-blue-600">
-        <i class="fa-solid fa-gamepad mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl"></i> Game Developer
+  <div class="pl-10 bg-gray-100 bg-opacity-70 rounded-md w-1/2">
+    <h1 class="text-center text-2xl md:text-4xl lg:text-8xl font-bold">F0rm1x</h1>
+    <div class="flex justify-center space-x-6 sm:text-xs md:text-l lg:text-xl font-bold">
+      <a href="#game-developer" class="flex items-center text-gray-700 hover:text-blue-800">
+        <i class="fa-solid fa-gamepad mr-2"></i> Game Developer
       </a>
-      <a href="#artist" class="flex items-center text-gray-700 hover:text-blue-600">
-        <i class="fa-solid fa-palette mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl"></i> Artist
+      <a href="#artist" class="flex items-center text-gray-700 hover:text-blue-800">
+        <i class="fa-solid fa-palette mr-2"></i> Artist
       </a>
-      <a href="#animator" class="flex items-center text-gray-700 hover:text-blue-600">
-        <i class="fa-solid fa-film mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl"></i> Animator
+      <a href="#animator" class="flex items-center text-gray-700 hover:text-blue-800">
+        <i class="fa-solid fa-film mr-2"></i> Animator
       </a>
     </div>
-    <p class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-left mt-6 mr-6">
+    <div class="text-2xl text-left">
 
-      Hi there!,<br>
       
-      Welcome to my new page.
-      DISCLAIMER: all the roles are here for the future. I am not an expert in any of them. I have just started learning, lol.
-    </p>
+      Beginner game developer, artist, and animator.<br>
+      I am currently learning game development, 2D art, and animation.<br>
+      DISCLAIMER: all the roles are here for the future. I am not good at an expert in any of them. I have just started, lol.<br>
+      <div style="margin-top: 100px;"></div>
+      <h2 class="text-xl md:text-xl lg:text-4xl font-bold">Game development</h2>
+      
+    </div>
   </div>
+
 </div>
